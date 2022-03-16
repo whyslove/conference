b8e62a50595a688b646befb4e4b93800c5865514
whyslove <svvoylov@edu.hse.ru>
2022-03-16 22:28:40 +0300

    button back and new reset_base_state with keyboard

--- telegram/core/handlers/moderator/__init__.py
+++ telegram/core/handlers/moderator/__init__.py
@@ -34,6 +34,9 @@ def setup(dp: Dispatcher):
     dp.register_message_handler(
         moderator_handlers.upload_xls, state="ready_upload_xls", content_types=["document"]
     )
+    dp.register_message_handler(
+        moderator_handlers.upload_xls, state="ready_upload_xls", regexp="Вернуться назад"
+    )
     dp.register_message_handler(general_schedule, regexp="Общее расписание", state="moderator_main")
     dp.register_message_handler(show_responses, regexp="Ответы участников", state="moderator_main")
 
